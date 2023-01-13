bdc459c2850d434dd07be8c6e40f8dd50610603a
E-gap <106596181+E-gap@users.noreply.github.com>
2023-01-13 14:19:42 +0200

    complete ContactForm

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,81 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import css from './ContactForm.module.css';
 import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
 
-class ContactForm extends React.Component {
-  state = {
-    name: '',
-    number: '',
+const ContactForm = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumder] = useState('');
+
+  const reset = () => {
+    setName('');
+    setNumder('');
   };
 
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
+  const handlerChangeName = event => {
+    setName(event.target.value);
   };
 
-  reset = () => {
-    this.setState({
-      name: '',
-      number: '',
-    });
+  const handlerChangeNumber = event => {
+    setNumder(event.target.value);
   };
 
-  handlerChangeName = event => {
-    this.setState({
-      name: event.target.value,
-    });
-  };
-
-  handlerChangeNumber = event => {
-    this.setState({
-      number: event.target.value,
-    });
-  };
-
-  handlerSubmit = event => {
+  const handlerSubmit = event => {
     event.preventDefault();
-    const contact = { ...this.state, id: uuidv4() };
-    this.props.onSubmit(contact);
-    this.reset();
+    const contact = { name, number, id: uuidv4() };
+    onSubmit(contact);
+    reset();
   };
 
-  render() {
-    return (
-      <form
-        className={`${css.contactForm} ${css.block}`}
-        onSubmit={this.handlerSubmit}
-      >
-        <label className={css.block}>
-          Name
-          <input
-            className={css.block}
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            value={this.state.name}
-            onChange={this.handlerChangeName}
-          />
-        </label>
+  return (
+    <form
+      className={`${css.contactForm} ${css.block}`}
+      onSubmit={handlerSubmit}
+    >
+      <label className={css.block}>
+        Name
+        <input
+          className={css.block}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          value={name}
+          onChange={handlerChangeName}
+        />
+      </label>
 
-        <label className={css.block}>
-          Number
-          <input
-            className={css.block}
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            value={this.state.number}
-            onChange={this.handlerChangeNumber}
-          />
-        </label>
-        <button type="Submit">Add contact</button>
-      </form>
-    );
-  }
-}
+      <label className={css.block}>
+        Number
+        <input
+          className={css.block}
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          value={number}
+          onChange={handlerChangeNumber}
+        />
+      </label>
+      <button type="Submit">Add contact</button>
+    </form>
+  );
+};
+
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
 
 export default ContactForm;
